63c87db03f443aaf856e0bd5c3ce3cd06a769231
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2019-11-19 16:14:15 +0200

    Initial commit

---
+++ Amount_set.c
@@ -0,0 +1,5 @@
+//
+// Created by User on 19-Nov-19.
+//
+
+#include "Amount_set.h"
---
+++ Amount_set.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 19-Nov-19.
+//
+
+#ifndef AMOUNT_SET__AMOUNT_SET_H_
+#define AMOUNT_SET__AMOUNT_SET_H_
+
+#endif //AMOUNT_SET__AMOUNT_SET_H_
