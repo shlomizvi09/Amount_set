01e940d47d2b1e1de6028f38d7d08688674c7330
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2019-11-19 22:39:02 +0200

    initial .c

--- Amount_set.c
+++ Amount_set.c
@@ -1,5 +1,19 @@
-//
-// Created by User on 19-Nov-19.
-//
 
 #include "Amount_set.h"
+
+typedef struct node_t {
+
+} *Node;
+
+typedef struct AmountSet_t {
+  CopyASElement copy_as_element_ = NULL;
+  FreeASElement free_as_element_ = NULL;
+  CompareASElements compare_as_elements_ = NULL;
+
+};
+
+AmountSet asCreate(CopyASElement copyElement,
+                   FreeASElement freeElement,
+                   CompareASElements compareElements) {
+
+}
