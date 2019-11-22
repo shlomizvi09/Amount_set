259415c72f7c9f48b028c759e924d68dbd7900c9
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2019-11-22 12:43:40 +0200

    initial .c

--- Amount_set.c
+++ Amount_set.c
@@ -1,19 +1,48 @@
 
 #include "Amount_set.h"
+#include <stdlib.h>
 
 typedef struct node_t {
-
+  ASElement element;
+  struct node_t *next;
 } *Node;
 
 typedef struct AmountSet_t {
-  CopyASElement copy_as_element_ = NULL;
-  FreeASElement free_as_element_ = NULL;
-  CompareASElements compare_as_elements_ = NULL;
-
+  CopyASElement as_copy;
+  FreeASElement as_free;
+  CompareASElements as_compare;
+  Node head;
 };
 
 AmountSet asCreate(CopyASElement copyElement,
                    FreeASElement freeElement,
                    CompareASElements compareElements) {
+  if (copyElement == NULL || freeElement == NULL || compareElements == NULL) {
+    return NULL;
+  }
+  AmountSet new_set = malloc(sizeof(*new_set));
+  if (new_set == NULL) {
+    return NULL;
+  }
+  new_set->as_compare = compareElements;
+  new_set->as_free = freeElement;
+  new_set->as_copy = copyElement;
+  new_set->head = malloc(sizeof(*(new_set->head)));
+  if (new_set->head == NULL) {
+    free(new_set);
+    return NULL;
+  }
+  new_set->head->next = NULL;
+  new_set->head->element = NULL;
+  return new_set;
+}
 
+AmountSetResult asRegister(AmountSet set, ASElement element) {
+  if ((set == NULL) || (element == NULL)) {
+    return AS_NULL_ARGUMENT;
+  }
+  Node iterator = set->head->next;
+  for(;iterator!=NULL;iterator = iterator->next){
+
+  }
 }
