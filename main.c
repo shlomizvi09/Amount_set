b29d62f98b52c037feca8b50925ff4be023acf27
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2019-11-25 22:58:15 +0200

    initial .c

--- main.c
+++ main.c
@@ -2,8 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+ASElement copyFunction(ASElement element) {
+  if (element == NULL) {
+    return NULL;
+  }
+  int *new_element = malloc(sizeof(int));
+  if (new_element == NULL) {
+    return NULL;
+  }
+  *new_element = *(int *) element;
+  return new_element;
+}
+ASElement freeFunction(ASElement element) {
+  free(element);
+}
+int compareFunction(ASElement a, ASElement b) {
+  int new_a = *(int *) a;
+  int new_b = *(int *) b;
+  return a > b ? 1 : (a < b ? -1 : 0);
+}
 
 int main() {
+
   return 0;
 }
