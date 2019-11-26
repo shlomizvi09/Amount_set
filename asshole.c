7959ab697d2d2d86f18500f60b49fbc0b9ec8cfb
Arik Berenshtein <57088417+olcaar@users.noreply.github.com>
2019-11-26 15:53:12 +0200

    Create asshole.c

---
+++ asshole.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct node_t {
+  ASElement element;
+  double amount;
+  struct node_t *next;
+} *Node;
+
+static Node getElementNodePtr(AmountSet set, ASElement element);
+
+typedef struct AmountSet_t {
+  CopyASElement as_copy;
+  FreeASElement as_free;
+  CompareASElements as_compare;
+  Node head;
+  Node iterator;
+} *AmountSet;
